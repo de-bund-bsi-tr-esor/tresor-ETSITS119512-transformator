bb986206b93dc805aded6e8ab166067d78a1d0d3
Florian Otto <florian.otto@ecsec.de>
2021-12-20 10:33:11 +0100

    Fix src headers

--- service/src/main/java/tresor/trans/service/client/TLSProvisioning.java
+++ service/src/main/java/tresor/trans/service/client/TLSProvisioning.java
@@ -1,3 +1,20 @@
+/****************************************************************************
+ * Copyright (c) 2020 Federal Office for Information Security (BSI), ecsec GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ***************************************************************************/
+
 
 
 package tresor.trans.service.client;
@@ -20,17 +37,6 @@ import org.apache.cxf.transport.http.HTTPConduit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tresor.trans.service.client.ClientConfig.TLSConfig;
-
-
-/****************************************************************************
- * Copyright (C) 2021 ecsec GmbH.
- * All rights reserved.
- * Contact: ecsec GmbH (info@ecsec.de)
- *
- * This file may be used in accordance with the terms and conditions
- * contained in a signed written agreement between you and ecsec GmbH.
- *
- ***************************************************************************/
 
 
 /**
