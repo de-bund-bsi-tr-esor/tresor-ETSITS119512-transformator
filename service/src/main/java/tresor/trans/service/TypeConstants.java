827b8e0b616f1fe2351d2ab2225a123b6e5222f4
Florian Otto <florian.otto@ecsec.de>
2022-01-11 09:12:40 +0100

    Update type constants uris

--- service/src/main/java/tresor/trans/service/TypeConstants.java
+++ service/src/main/java/tresor/trans/service/TypeConstants.java
@@ -26,14 +26,14 @@ import java.util.Set;
  */
 public class TypeConstants {
 
-	public static final String ETSI_512_API_NS = "http://uri.etsi.org/19512/v1.1.2#";
-	public static final String TRESOR_API_NS = "http://www.bsi.bund.de/tr-esor/api/1.2";
-	public static final String TRESOR_VR_NS = "http://www.bsi.bund.de/tr-esor/vr/1.2";
+	public static final String ETSI_512_API_NS = "http://uri.etsi.org/19512/v1.1.3#";
+	public static final String TRESOR_API_NS = "http://www.bsi.bund.de/tr-esor/api/1.3";
+	public static final String TRESOR_VR_NS = "http://www.bsi.bund.de/tr-esor/vr/1.3";
 
-	public static final String XAIP_TYPE = "http://www.bsi.bund.de/tr-esor/xaip/1.2";
+	public static final String XAIP_TYPE = "http://www.bsi.bund.de/tr-esor/xaip/1.3";
 	public static final String LXAIP_TYPE = "http://www.bsi.bund.de/tr-esor/lxaip/1.3";
-	public static final String DXAIP_TYPE = "http://www.bsi.bund.de/tr-esor/dxaip/1.2";
-	public static final String DLXAIP_TYPE = "http://www.bsi.bund.de/tr-esor/dlxaip/1.2";
+	public static final String DXAIP_TYPE = "http://www.bsi.bund.de/tr-esor/dxaip/1.3";
+	public static final String DLXAIP_TYPE = "http://www.bsi.bund.de/tr-esor/dlxaip/1.3";
 	public static final String ASIC_TYPE = "http://uri.etsi.org/ades/ASiC/type/ASiC-ERS";
 	public static final String CADES_TYPE = "http://uri.etsi.org/ades/CAdES";
 	public static final String XADES_TYPE = "http://uri.etsi.org/ades/XAdES";
@@ -41,7 +41,7 @@ public class TypeConstants {
 	public static final String ASICE_TYPE = "http://uri.etsi.org/ades/ASiC/type/ASiC-E";
 	public static final String DIGESTLIST_TYPE = "http://uri.etsi.org/19512/format/DigestList";
 
-	public static final String BINARYDATA_TYPE = "http://www.bsi.bund.de/tr-esor/api/1.2/type/binaryData";
+	public static final String BINARYDATA_TYPE = "http://www.bsi.bund.de/tr-esor/api/1.3/type/binaryData";
 
 	public static final String ERS_RFC_4998 = "urn:ietf:rfc:4998:EvidenceRecord";
 	public static final String ERS_RFC_6283 = "urn:ietf:rfc:6283:EvidenceRecord";
@@ -50,6 +50,6 @@ public class TypeConstants {
 	public static final String S4_ERS_RFC_4998 = "urn:ietf:rfc:4998";
 	public static final String S4_ERS_RFC_6283 = "urn:ietf:rfc:6283";
 
-	public static final Set<String> SUPPORTED_XPATH_QUERY = Set.of("http://www.bsi.bund.de/tr-esor/api/1.2/query/xpath");
+	public static final Set<String> SUPPORTED_XPATH_QUERY = Set.of("http://www.bsi.bund.de/tr-esor/api/1.3/query/xpath");
 
 }
